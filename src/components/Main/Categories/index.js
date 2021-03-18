29da842f69c93664c60e57924616a1743adb2a1c
edubaccarezza <edubaccarezza@gmail.com>
2021-03-18 19:49:35 -0300

    ultimos arreglos

--- src/components/Main/Categories/index.js
+++ src/components/Main/Categories/index.js
@@ -6,7 +6,7 @@ export default function Categories () {
 		<div className="col-lg-6 mb-4">						
 			<div className="card shadow mb-4">
 				<div className="card-header py-3">
-					<h6 className="m-0 font-weight-bold text-primary">Categories in Data Base</h6>
+					<h6 className="m-0 font-weight-bold text-primary">Productos por categorías</h6>
 				</div>
 				<div className="card-body">
 					<Category />
